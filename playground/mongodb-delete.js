3be794ed3dff4e2d483673ccefa795382606a0dc
Vladimir Sarac <vsarac@spel.local>
2018-10-08 15:41:41 +0200

    Init commit 5

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,25 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+//conecting to TodoApp database and deleting rows  
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err){ return console.log('Unable to connect to MongoDB server'); }
+  console.log('Connected to MongoDB server');
+  //deleteMany
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+  // 	console.log(result);
+  // });
+  //deleteOne
+  db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  	console.log(result);
+  });
+
+  //close the connection
+  // db.close();
+});
+
+
+
+
+
+
+
