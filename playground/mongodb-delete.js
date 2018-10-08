1d754b63fec7ae52f2b65c193a7267dcdbfee6fb
Vladimir Sarac <vsarac@spel.local>
2018-10-08 15:52:58 +0200

    Init commit 7

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -9,7 +9,18 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   // 	console.log(result);
   // });
   //deleteOne
-  db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  // 	console.log(result);
+  // });
+  //findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  // 	console.log(result);
+  // });
+  //brisanje u Users collection-u
+  db.collection('Users').deleteMany({ name: 'Mile' }).then((result) => {
+  	console.log('bla');
+  });
+  db.collection('Users').findOneAndDelete({ _id: new ObjectID("5bbb3d0af453aadbc81490da") }).then((result) => {
   	console.log(result);
   });
 
