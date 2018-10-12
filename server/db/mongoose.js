053a7b97cb9b9a33c6dfca881ee5e4bb2e9299a7
Vladimir Sarac <vsarac@spel.local>
2018-10-12 11:55:21 +0200

    ready for heroku2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');//uvozimo paket mongoose za rad sa mongoDB bazom
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');//povezivanje sa bazom
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');//povezivanje sa bazom
 
 module.exports = {
   mongoose: mongoose	
